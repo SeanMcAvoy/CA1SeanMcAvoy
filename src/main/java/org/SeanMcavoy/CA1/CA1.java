2d517f8095614895d63927e23b0a295e6c704559
seanmcavoy <smcavoy.mcavoy@gmail.com>
2020-10-20 21:06:11 +0100

    Before Trying to find the max and second max

--- src/main/java/org/SeanMcavoy/CA1/CA1.java
+++ src/main/java/org/SeanMcavoy/CA1/CA1.java
@@ -13,24 +13,42 @@ public class CA1
      * @param grades
      * @return array of the top 5 Results
      */
-    public int [] selectFiveGrades(int [] codes, int [] grades)
+    public static int [] selectFiveGrades(int [] codes, int [] grades)
     {
         //Stores the Five highest Grades excluding CSPE
         int [] selectedGrades = new int[5];
 
 
+        for (int i = 0; i < codes.length ; i++)
+        {
+            int n = 0; // Stop elements replacing each other.
+            //adding Math, English and Irish to the array
+            if(codes[i] == 1 || codes[i] == 2 || codes[i] == 3)
+            {
+                grades[i] = selectedGrades[n];
+                n++;
+            }
+        }
+
+
+
         return selectedGrades;
     }
 
-    private double calulateAverage(int[] selectedGrades)
+    /**
+     * Calulates Average of the Grades
+     * @param selectedGrades from method selectFiveGrades()
+     * @return average as Double
+     */
+    private static double calulateAverage(int[] selectedGrades)
     {
         int sum = 0;
         for (int elements : selectedGrades)
         {
             sum += elements;
         }
-
-        double average = sum / selectedGrades.length;
+        //needs the (double) as sum is going from int to double
+        double average = (double) sum / selectedGrades.length;
         return average;
     }
 }
