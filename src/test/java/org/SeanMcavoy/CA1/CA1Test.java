11afb4ddf6c4c5ef30002aaf23ee62f7b2d892b7
seanmcavoy <smcavoy.mcavoy@gmail.com>
2020-10-23 21:52:00 +0100

    First Testing running and successful

--- src/test/java/org/SeanMcavoy/CA1/CA1Test.java
+++ src/test/java/org/SeanMcavoy/CA1/CA1Test.java
@@ -3,14 +3,33 @@ package org.SeanMcavoy.CA1;
 import junit.framework.TestCase; //
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+
 public class CA1Test extends TestCase
 {
 
     /**
-     * All Test For SelectFiveGrades()
+     * All Test For SelectFiveGrades()- Test takes math eng irish in & next two max, test doesnt take cepe etc...
      */
     @Test
-    public void testSelectFiveGrades() {
+    public void testSelectFiveGrades()
+    {
+        System.out.println("selectFiveGrades");
+        int[] codes = {1,2,3,4,5,6,7,8};
+        int[] grades = {1,50,3,4,5,0,0,0};
+        int[] expResult = new int [] {1,50,3,5,4};
+        int[] result = CA1.selectFiveGrades(codes,grades);
+        assertArrayEquals(expResult, result);
+    }
+    /**@Test
+    public void testSelectFiveGrades1()
+    {
+        System.out.println("selectFiveGrades");
+        int[] codes = null;
+        int[] grades = null;
+        int[] expResult = null;
+        int[] result = CA1.selectFiveGrades(codes,grades);
+        assertArrayEquals(expResult, result);
     }
 
     @Test
@@ -24,4 +43,5 @@ public class CA1Test extends TestCase
     @Test
     public void testProcessStudentRecord() {
     }
+    */
 }
